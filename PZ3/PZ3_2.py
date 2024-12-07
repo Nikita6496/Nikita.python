f92f81ae6d060aa0419abc5f261f0da72cded14e
Nikita6496 <Nikita6496>
2024-12-07 15:06:05 +0300

    Практическая работа 3_2

--- PZ3/PZ3_2.py
+++ PZ3/PZ3_2.py
@@ -1,25 +1,29 @@
+#Даны три числа. Найти среднее из них
+
 a = input("Первое число:")
 b = input("Второе число:")
 c = input("Третье число:")
-while type(a) != int: #Обработка исключений
+
+#Обработка ислючений
+while type(a) != int:
     try:
         a = int(a)
     except ValueError:
         print("Ошибка")
         а = input("Введите целое число:")
-while type(b) != int: #Обработка исключений
+while type(b) != int:
     try:
         b = int(b)
     except ValueError:
         print("Ошибка")
         b = input("Введите целое число:")
-while type(c) != int: #Обработка исключений
+while type(c) != int:
     try:
         c = int(c)
     except ValueError:
         print("Ошибка")
         c = input("Введите целое число:")
-#(a>b>c or c>b>a or b>a>c or c>a>b or b>c>a or a>c>b)
+
 if a>b>c or c>b>a:
     print(b)
 elif b>a>c or c>a>b:
