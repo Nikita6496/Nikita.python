73bf85a3909d68583cc5584a1fc686abe6aa886a
Nikita6496 <nikitapasenko720@gmail.com>
2024-12-12 18:28:58 +0300

    Практическая работа 4_1

--- PZ4/PZ4_1.py
+++ PZ4/PZ4_1.py
@@ -1,1 +1,32 @@
-print("AA")
+# Дано вещественное число Х и целое число N (>0).
+# Найти значение выражения 1 + X + (X ^ 2) / (2!) +...+X^ N /(N!)(N!=12...N)
+X = input("Введите вещественное число X: ")
+N = input("Введите целое число N (N > 0): ")
+
+# Обработка исключений
+while type(X) != float:
+    try:
+        X = float(X)
+    except ValueError:
+        print("Ошибка!")
+        X = input("Введите вещественное число X: ")
+while type(N) != int:
+    try:
+        N = int(N)
+    except ValueError:
+        print("Ошибка!")
+        N = input("Введите целое число N (N > 0): ")
+
+h = 1
+s = 1
+result = 0
+if N > 0:
+    while s <= N:
+        h *= s
+        g = (X ** s) / h
+        s += 1
+        result += g
+
+    print(1 + result)
+else:
+    print("Ошибка ввода, перезапустите программу и введите N > 0")
