e6a4bc73e10a6525fce1b4021353362a541742c0
Nikita6496 <nikitapasenko720@gmail.com>
2024-12-13 15:05:31 +0300

    Практическая работа 4_1

---
+++ PZ5/PZ5_1.py
@@ -0,0 +1,22 @@
+import random
+num = 0
+
+def generate(a):
+    a = random.randint(1000, 9999)
+    return a
+b = (generate(num))
+print (f"Число:{b}")
+
+def number_povtor(c):
+    if 1000 <= b <= 9999:  # Проверка числа
+        f = b // 1000
+        g = (b // 10) % 10
+        d = (b // 100) % 10
+        r = b % 10
+        c = f == g or f == d or f == r or g == d or g == r or d == r
+        return c
+p = (number_povtor(b))
+print (f"Число имеет одинаковые цифры? {p}")
+
+
+
