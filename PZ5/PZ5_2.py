32de2ea9ace3cdbb5d514ef645ca728e773f6674
Nikita6496 <nikitapasenko720@gmail.com>
2024-12-14 09:32:36 +0300

    Практическая 5_2

--- PZ5/PZ5_2.py
+++ PZ5/PZ5_2.py
@@ -1,13 +1,18 @@
+# Описать функцию PowerA234(A, B, C, D),
+# вычисляющую вторую, третью и четвёртую степень числа A и возвращающую эти степени.
+# С помощью этой функции найти 2, 3, 4 степень пяти данных чисел
+
 def PowerA234(A, B = 2, C = 3, D = 4):
     B = A ** B
     C = A ** C
     D = A ** D
     return A, B, C, D
+
 def foonk(o = 5):
     i = 1
     while i <= o:
         num = input("Введите ваше число: ")
-        while type(num) != float:
+        while type(num) != float: # Проверка исключений
             try:
                 num = float(num)
             except ValueError:
