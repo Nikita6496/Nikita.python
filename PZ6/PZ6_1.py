8c9a680930a8254f7959d835b543735cbfd9449b
Nikita6496 <nikitapasenko720@gmail.com>
2024-12-17 17:10:31 +0300

    Практическая работа 6_1

--- PZ6/PZ6_1.py
+++ PZ6/PZ6_1.py
@@ -1,11 +1,26 @@
+# Дан целочисленный список A размером 10. Вывести порядковый номер последнего из тех его элементов Ak,
+# которые удовлетворяют двойному неравенству A1 < Ak < A10.
+# Если таких элементов нет, то вывести 0.
+
 import random
-c = []
-i = 0
-while i < 10:
-    c.append(random.randint(0,100))
-    i += 1
-print(c)
-for a in c:
-    if c[0] < a <c[-1]:
-        break
-print(a)
+list = []
+i_1 = 0
+while i_1 < 10:
+    list.append(random.randint(0,100))
+    i_1 += 1
+
+print(list)
+h = 1
+proverka = 0
+index = 0
+while h < 10:
+    for i_2 in list:
+        if h == 10 and proverka > 0:
+            number = index + 1
+            print(number)
+        if list[0] < i_2 < list[-1]:
+            index = list.index(i_2)
+            proverka += 1
+        h += 1
+    if proverka < 1:
+        print(0)
