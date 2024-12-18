7dfc3f7bbfd5bc642c2e6034a2f0bbe85ffa0dd5
Nikita6496 <nikitapasenko720@gmail.com>
2024-12-18 19:27:12 +0300

    Практическая работа 6_2

--- PZ6/PZ6_2.py
+++ PZ6/PZ6_2.py
@@ -2,12 +2,7 @@
 import random
 a = []
 R = input("Введите ваше целое число: ")
-N = input("Введите размер списка: ")
-i = 1
-
-#if R < 0:
-    #print("Ошибка! Число R должно быть больше 0")
-    #R = input("Введите ваше целое положительное число: ")
+N = input("Введите размер списка (N > 1): ")
 
 while type(R) != int: # Обработка исключений
     try:
@@ -16,16 +11,42 @@ while type(R) != int: # Обработка исключений
         print("Ошибка!")
         R = input("Введите ваше число R: ")
 
+
 while type(N) != int: # Обработка исключений
     try:
         N = int(N)
+        if N < 2:
+            print("Ошибка!")
+            N = input("Введите размер списка N: ")
     except ValueError:
         print("Ошибка!")
         N = input("Введите размер списка N: ")
 
-while i <= N:
-    b = random.randint(-100,100)
-    a.append(b)
-    i += 1
-    
-print(f'Список сгенерирован = {a}')
+def proverka1(m):
+    for i in range(m):
+        a.append(random.randint(-100,100))
+        # c = len(a)
+        # print(c)
+
+proverka1(N)
+print(f"Список сгенерирован = {a}")
+
+def ggg(a, R):
+    h = 0
+    pair = 0
+    for i in range(N):
+        for j in range(i + 1, N):
+            sum = a[i] + a[j]
+            k = sum - R
+            if k < h:
+                h = k
+                pair = [i, j]
+    if pair != 0:
+        i, j = pair
+        if i < j:
+            return a[i], a[j]
+    #else:
+        #return 0
+
+result = ggg(a, R)
+print(result)
