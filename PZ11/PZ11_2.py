90f27487d751601c9ba1a4a35ff222c509f537d8
yk6SIXKilla <cheakibreaki@gmail.com>
2025-03-14 16:29:56 +0300

    Практическая работа 11_2

--- PZ11/PZ11_2.py
+++ PZ11/PZ11_2.py
@@ -1,7 +1,28 @@
-numbers = [10, 11, 12, 13, 14, 15, -16]
-count = len(numbers)
-new_s = []
-for i in range(1, count -1):
-    new_e = (numbers[i - 1] + numbers[i + 1]) ** 2
-    new_s.append(new_e)
-print(new_s)
+#Из предложенного текстового файла (text18-18.txt) вывести на экран его содержимое,
+#количество знаков пунктуации в первых четырёх строках. Сформировать новый файл, в
+#который поместить текст в стихотворной форме выведя строки в обратном порядке.
+t = 0
+d = 0
+m = 0
+for i in open('text18-18.txt', encoding='UTF-16'):
+ print(i, end='')
+ #while t != 4:
+  #for h in i:
+   #if h == "." or h == "—" or h == "..." or h == ",":
+    #m += 1
+ t += 1
+ for j in i:
+  if j == 'ж':
+   d += 1
+print(end='\n')
+print('Количество строк: ', t, end='\n')
+print('Количество букв "ж" : ', d, end='\n')
+
+f1 = open('text18-18.txt', encoding='UTF-16')
+l = f1.readlines()
+l[0], l[3] = l[3], l[0]
+f1.close()
+
+f2 = open('text18-18-2.txt', 'w')
+f2.writelines(l)
+f2.close()
