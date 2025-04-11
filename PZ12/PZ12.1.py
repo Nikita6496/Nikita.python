4d55b222034831c4a24bb3cb01cfc4733c94cb23
Nikita6496 <nikitapasenko720@gmail.com>
2025-04-11 16:21:05 +0300

    Практическая работа 12_1

--- PZ12/PZ12.1.py
+++ PZ12/PZ12.1.py
@@ -1,7 +1,12 @@
+#В последовательности на n целых элементов найти произведение элементов
+#средней трети.
+
 import random
 from functools import reduce
+
 n = int(input("Введите количество элементов: "))
 a = [random.randint(1, 100) for i in range(n)]
 print(a)
+
 nikita = [a[b] * (a[b + 1]) for b in range (int(len(a) / 3), int(len(a) - len(a) / 3) - 1)]
 print(reduce(lambda x,y: x * y, nikita))
