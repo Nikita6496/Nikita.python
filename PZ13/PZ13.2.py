bd3cc10cf0eaed08f011096816258af0fa07dd8f
Nikita6496 <nikitapasenko720@gmail.com>
2025-05-31 12:44:34 +0300

    ПЗ13

--- PZ13/PZ13.2.py
+++ PZ13/PZ13.2.py
@@ -1,8 +1,11 @@
 # В двумерном списке найти среднее арифметическое элементов последних двух
 # столбцов.
 import random
+from functools import reduce
 spisok = [[random.randint(1, 10) for i in range(3)] for i in range(3)]
 for i in spisok:
     print(i)
-a = list(map(lambda x:(x[1] + x[2]) / 2, spisok))
+a = list(map(lambda x: sum(x[-2:]), spisok))
 print(a)
+b = reduce(lambda x,y: (x+y), a)
+print(b / 3)
